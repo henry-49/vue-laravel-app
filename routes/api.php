63a6eb73767240d5c0e80a2e234d77dc1173bfde
henry-49 <henryswebdevelopment@gmail.com>
2025-08-29 00:09:17 +0200

    feat: API-Route /api/ideen mit optionalem Kategorie-Filter

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Idea;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/ideen', function (Request $request) {
+    $query = Idea::query();
+
+    if ($request->has('category')) {
+        $query->where('category', $request->category);
+    }
+
+    return $query->latest()->get([
+        'title',
+        'description',
+        'category',
+        'created_at as date',
+    ]);
 });
